efb2c09ad05a10b715cc5615bef7a42f2ffab61e
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2025-12-01 17:21:19 +0100

    read file and tests

--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 require_relative "fcm/version"
-
+require "debug"
 module Fcm
-
   def self.run
-    puts "hi"
+    file = ARGV.last
+    read_file(file) if file && file.split(".").last == "txt"
+  end
+
+  def self.read_file(file)
+    filedata = File.readlines(file)
+    filedata.select { |l| l.include?("SEGMENT") }
   end
 
   class Error < StandardError; end
