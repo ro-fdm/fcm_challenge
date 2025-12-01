f333d77a76c67d7faa551d8163d857f8ba760806
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2025-12-01 13:47:23 +0100

    create bundle gem

---
+++ lib/fcm.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fcm/version"
+
+module Fcm
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/fcm_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Fcm do
+  it "has a version number" do
+    expect(Fcm::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
